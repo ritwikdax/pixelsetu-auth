e5e63b73b9c881390b66ffb97263cd084f6a131d
Ritwik <dasritwik16@gmail.com>
2026-04-16 15:00:26 +0530

    fix: updated

--- src/handlers/logoutHandler.ts
+++ src/handlers/logoutHandler.ts
@@ -1,8 +1,19 @@
-import e, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { sessionManager } from "../services/session.servcie.js";
 
-export default async function logoutHandler(req: Request, res: Response){
-    const { encryptedSessionKey } = req.body;
-    await sessionManager.logoutUserSession(encryptedSessionKey);
-    return res.status(200).json({ success: true, message: "Logged out successfully" });
+export default async function logoutHandler(req: Request, res: Response) {
+  const { encryptedSessionKey } = req.body;
+  await sessionManager.logoutUserSession(encryptedSessionKey);
+  return res
+    .clearCookie("session_id", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      ...(process.env.NODE_ENV === "production" && {
+        domain: ".pixelsetu.com",
+      }),
+      path: "/",
+    })
+    .status(200)
+    .json({ success: true, message: "Logged out successfully" });
 }
